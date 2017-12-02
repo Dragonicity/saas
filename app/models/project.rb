af8eccee08be3ab958abe8583d373490aed7e38e
Mark-Nelson <mark.nelson@dragonicity.com>
2017-12-02 15:40:53 +0800

    users/projects controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,10 @@ class Project < ApplicationRecord
   belongs_to :tenant
   has_many :adtifacts, dependent: :destroy
 
+  has_many :user_projects
+  has_many :users, through: :user_projects
+
+
   validates_uniqueness_of :title
   validate :free_plan_can_only_have_one_project
 
@@ -24,7 +28,7 @@ class Project < ApplicationRecord
       if user.is_admin?
         tenant.projects.order(:id).limit(1)
       else
-         user.projects.where(tenant_id: tenant.id).order(:id).linit(1)
+         user.projects.where(tenant_id: tenant.id).order(:id).limit(1)
       end 
     end
   end
